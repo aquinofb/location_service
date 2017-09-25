9b22908a0470c0bb7be78356e42bb7be910e383f
Felipe Aquino <aquinofb@gmail.com>
2017-09-25 09:32:31 +0100

    Update location_finder.go

--- clients/location_finder.go
+++ clients/location_finder.go
@@ -12,15 +12,8 @@ import (
 func LocationFinder(lat, lng float32, location_type string) []models.Location {
   switch services.CountryIsoCode(lat, lng) {
   case "GB":
-    fmt.Println("It's GB!!!")
     return nhs.LocationFinder(lat, lng, location_type)
   default:
-    fmt.Println("It's not GB :'(")
     return google.LocationFinder(lat, lng, location_type)
   }
-  
-  // if (service?) {
-  //   getpostcode
-  //   NHSService.by_postcode()
-  // } else if (organisation?)
 }
